20d4bd9849728f194e579a44e37fd4359666966e
zzyin66 <71182874+zzyin66@users.noreply.github.com>
2021-10-27 16:32:33 -0400

    Update test.php

--- test.php
+++ test.php
@@ -1,3 +1,12 @@
 <?php
+$array = array("1" => "PHP code tester Sandbox Online",  
+              "foo" => "bar", 5 , 5 => 89009, 
+              "case" => "Random Stuff: " . rand(100,999),
+              "PHP Version" => phpversion()
+              );
+              
+foreach( $array as $key => $value ){
+    echo $key."\t=>\t".$value."\n";
+}
 
 echo('test');
